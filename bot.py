767ad1de75751059a1248d8386043d00cdb89024
pineafan <pineapplefanyt@gmail.com>
2021-08-22 12:10:28 +0100

    add files

---
+++ bot.py
@@ -0,0 +1,17 @@
+import discord
+from config import Bot
+intents = discord.Intents.default()
+intents.members = True
+
+bot = Bot(
+    owner_ids=[
+        317731855317336067,
+        421698654189912064,
+        487443883127472129,
+        438733159748599813
+    ],
+    case_insensitive=True,
+    presence=None,
+    intents=intents
+)
+bot.requests = {}
